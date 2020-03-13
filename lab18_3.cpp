7cae5fba1684a36cd4f13a4660933914907e3eeb
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-03-13 16:14:41 +0700

    Update lab18_3.cpp

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -9,6 +9,10 @@ using namespace std;
 
 struct student{
 	//Define struct student with four member (name ,id , gender, gpa);
+	string name;
+	int id;
+	char gender;
+	float gpa;
 };
 
 struct course{
@@ -18,7 +22,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //Correct this line
+student * findstudent(vector<student> &allstudents,int key){ //Correct this line
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -56,9 +60,11 @@ int main(){
 	
 	while(getline(student_file,textline)){
 		student s; 
-	
+		char name[100];
+		sscanf(textline.c_str(),"%[^,],%d,%c,%f",name,&s.id,&s.gender,&s.gpa);
+		cout << s.name << " " << s.id << " " << s.gender << " " << s.gpa << endl;
 		//Assign value to the members of struct s;
-	
+		s.name = name;
 		allstudents.push_back(s); 		
 	}
 	
@@ -77,6 +83,7 @@ int main(){
 				state = 3;
 			}else{
 				//Append lecture_list;
+				allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
@@ -84,6 +91,7 @@ int main(){
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
 				//Append student_list;
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 			}
 		}
 	}
