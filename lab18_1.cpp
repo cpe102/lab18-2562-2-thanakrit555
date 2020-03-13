009cdf87d3c1a6621a7e21fe8da539d9d6952ae3
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-03-13 16:37:11 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -3,38 +3,35 @@ using namespace std;
 
 struct Rect{
 	double x,y,w,h;
+	
 };
 
-double overlap(Rect R,Rect r){
-  
-	double r1_x=R1.x+R1.w;
-	double r2_x=R2.x+R2.w;
-	
-	cout<<min(r1_x,r2_x)-max(R1.x-R2.x);
-	
-	Rect r1_y=R1.y-R1.h;
-	Rect r2_y=R2.y-R2.h;
-	
-	cout<<max(r2_y,r1_y)-min(R2.y,R1.y);
-}
+double overlap(Rect A,Rect B){
+	   
+	   double x1=max(A.x,B.x);
+	   double x2=min(A.x+A.w,B.x+B.w);
+	   double x =x2-x1;
 
-int main(){
-	Rect R;
-	Rect r;
+	   double y1=min(A.y,B.y);
+	   double y2=max(A.y-A.h,B.y-B.h);
+	   double y =y1-y2;
+
+	if(x*y >0)
+	{
+		return x*y;
+	}else 
+	{
+		return 0;
+	}
+	  }
+
+int main()
+{
+	Rect s1,s2;
 	cout << "Please input Rect 1 (x y w h): ";
-	cin>>R1.x;
-	cin>>R1.y;
-	cin>>R1.w;
-	cin>>R1.h;
+	cin  >>s1.x >>s1.y >>s1.w >>s1.h;
 	cout << "Please input Rect 2 (x y w h): ";
-	cin>>r1.x;
-	cin>>r1.y;
-	cin>>r1.w;
-	cin>>r1.h;
-    
-
-
-	
-	cout << "\nOverlap area = ";	
+	cin  >>s2.x >>s2.y >> s2.w >> s2.h;
+	cout << "Overlap area =  "<< overlap(s1,s2);	
 	return 0;
 }
